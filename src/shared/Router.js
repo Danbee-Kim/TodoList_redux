dfa44a7bc4b65f81059f07532baef42770741e92
Danbee Kim <kdb2360@gmail.com>
2022-08-03 16:54:03 +0900

    second commit

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 // 1. react-router-dom을 사용하기 위해서 아래 API들을 import 합니다.
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Detail from "../components/Detail";
+import DetailPage from "../pages/DetailPage";
 import TodoList from "../pages/TodoList";
 
 // 2. Router 라는 함수를 만들고 아래와 같이 작성합니다.
@@ -10,8 +10,8 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TodoList />} />
-        <Route path="detail/" element={<Detail />} />
-        <Route path="detail/:id" element={<Detail />} />
+        <Route path="detail/" element={<DetailPage />} />
+        <Route path="detail/:id" element={<DetailPage />} />
       </Routes>
     </BrowserRouter>
   );
